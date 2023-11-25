fe9f3ef4765576966f079f1aab2a385714026c70
Lamees Shalaby <lameesshalaby@192.168.1.69>
2023-11-25 17:18:42 +0200

    .

--- instagram-clone/src/Components/Timeline/Posts/Posts.jsx
+++ instagram-clone/src/Components/Timeline/Posts/Posts.jsx
@@ -1,8 +1,42 @@
-import './Posts.css'
-const Posts = () => {
-  return (
-    <div className='post'>Posts</div>
-  )
-}
+import { Avatar } from "@mui/material";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import "./Posts.css";
 
-export default Posts
+function Posts({ user, postImage, likes, timestamp }) {
+  return (
+    <div className="post">
+      <div className="post-header">
+        <div className="post-headerAuthor">
+          <Avatar style={{ marginRight: "10px" }}>
+            {user.charAt(0).toUpperCase()}
+          </Avatar>
+          {user} • <span>{timestamp}</span>
+        </div>
+        <MoreHorizIcon />
+      </div>
+      <div className="post-image">
+        <img src={postImage} alt="Post Image" />
+      </div>
+      <div className="post-footer">
+        <div className="post-footerIcons">
+          <div className="post-iconsMain">
+            <FavoriteBorderIcon className="postIcon" />
+            <ChatBubbleOutlineIcon className="postIcon" />
+            <TelegramIcon className="postIcon" />
+          </div>
+          <div className="post-iconSave">
+            <BookmarkBorderIcon className="postIcon" />
+          </div>
+        </div>
+        Liked by {likes} people.
+      </div>
+    </div>
+
+  );
+};
+
+export default Posts;
--- instagram-clone/src/Components/Timeline/Suggestions/Suggestions.jsx
+++ instagram-clone/src/Components/Timeline/Suggestions/Suggestions.jsx
@@ -1,9 +1,67 @@
-import './Suggestions.css'
+import { Avatar } from "@mui/material";
+import "./Suggestions.css";
 
-const Suggestions = () => {
+function Suggestions() {
+
   return (
-    <div>Suggestions</div>
-  )
+    <div className="suggestions">
+      <div className="suggestions-title">Suggestions for you</div>
+      <div className="suggestions-usernames">
+        <div className="suggestions-username">
+          <div className="username-left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username-info">
+              <span className="username">redian_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow-button">Follow</button>
+        </div>
+
+        <div className="suggestions-username">
+          <div className="username-left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username-info">
+              <span className="username">redian_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow-button">Follow</button>
+        </div>
+
+        <div className="suggestions-username">
+          <div className="username-left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username-info">
+              <span className="username">redian_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow-button">Follow</button>
+        </div>
+
+        <div className="suggestions-username">
+          <div className="username-left">
+            <span className="avatar">
+              <Avatar>R</Avatar>
+            </span>
+            <div className="username-info">
+              <span className="username">redian_</span>
+              <span className="relation">New to Instagram</span>
+            </div>
+          </div>
+          <button className="follow-button">Follow</button>
+        </div>
+      </div>
+    </div>
+  );
+
 }
 
 export default Suggestions

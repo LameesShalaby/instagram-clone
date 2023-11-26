2c5c79e136153d941bff4fa75c5335b7fa26562b
LameesShalaby <lameesshalaby254@gmail.com>
2023-11-26 17:34:15 +0200

    .

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,10 +1,43 @@
-import './App.css';
+import "./App.css";
 import Home from './Pages/Home';
+import Authenticate from './Authenticate/Authenticate';
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { loginUser, setLoading } from "./features/userSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          loginUser({
+            uid: authUser.uid,
+            username: authUser.displayName,
+            email: authUser.email,
+          })
+        );
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+        console.log("User is not logged in.");
+      }
+    });
+  }, []);
+
+  const user = useSelector((state) => state.data.user.user);
+  const isLoading = useSelector((state) => state.data.user.isLoading);
   return (
-    <div className="App">
-      <Home/>
+    <div className="app">
+      {isLoading ? (
+        <div class="loader-container">
+          <div class="loader"></div>
+        </div>
+      ) : (
+        <>{user ? <Home /> : <Authenticate />}</>
+      )}
     </div>
   );
 }
--- instagram-clone/src/Authenticate/Authenticate.jsx
+++ instagram-clone/src/Authenticate/Authenticate.jsx
@@ -1,9 +1,43 @@
-import './Authenticate.css'
+import { useState } from "react";
+import "./Authenticate.css";
+import Login from "./Login";
+import Signup from "./SignUp";
 
-const Authenticate = () => {
+function Authenticate() {
+  const [active, setActive] = useState("login");
+
+  const handleChange = () => {
+    setActive(active === "login" ? "signUp" : "login");
+  };
+
   return (
-    <div>Authenticate</div>
-  )
+    <div className="authenticate">
+      <div className="auth-left">
+        <img
+          src="https://i.imgur.com/P3Vm1Kq.png"
+          alt="Instagram Screenshots"
+        />
+      </div>
+      <div className="auth-right">
+        {active === "login" ? <Login /> : <Signup />}
+
+        <div className="auth-more">
+          <span>
+            {active === "login" ? (
+              <>
+                Don't have an account?{" "}
+                <button onClick={handleChange}>Sign Up</button>
+              </>
+            ) : (
+              <>
+                Have an account? <button onClick={handleChange}>Log in</button>
+              </>
+            )}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Authenticate
+export default Authenticate;
